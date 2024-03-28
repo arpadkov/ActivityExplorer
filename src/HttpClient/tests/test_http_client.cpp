a5f5c49110200f3a142d10e99886a15ac352f3fa
95arp_Dekstop <95.arpadkov@gmail.com>
2024-03-28 08:09:55 +0100

    Setup QApplication for GTest, start testing HttpClient::waitForReply

--- src/HttpClient/tests/test_http_client.cpp
+++ src/HttpClient/tests/test_http_client.cpp
@@ -1,11 +1,56 @@
 #include <gtest/gtest.h>
 
 #include "HttpClient.h"
+#include <NetworkRequest.h>
+
+#include <QtWidgets/QApplication>
 
 
 TEST(TestHttpClient, TestGetClient) {
+	// Must get the same pointer
 	auto client1 = HttpClient::get();
 	auto client2 = HttpClient::get();
 
 	EXPECT_EQ(client1, client2);
 }
+
+TEST(TestHttpClient, TestWaitForReply)
+{
+	auto client = HttpClient::get();
+
+	const QString test_post_url = "https://httpbin.org/post";
+
+	NetworkRequest request(test_post_url);
+	request.addQueryItem("item1", "value1");
+
+	ErrorDetail error;
+	auto reply = client->waitForReply(request, error, 3 * 1000);
+
+	EXPECT_TRUE(reply);
+	
+
+	auto value = reply->getValue("args");
+	std::cout << value.toStdString();
+	std::cout << reply->getRawData().toStdString();
+
+
+
+	//const auto& access_token = reply->getValue(ACCESS_TOKEN);
+	//if (access_token.isEmpty())
+	//	throw std::runtime_error(QString("Reply did not contain access token").toStdString());
+
+	//_access_token = access_token;
+	//qInfo() << "ACCESS TOKEN: " << _access_token;
+	//return true;
+
+	EXPECT_TRUE(false);
+}
+
+int main(int argc, char* argv[])
+{
+	::testing::InitGoogleTest(&argc, argv);
+	QApplication a(argc, argv);
+	return RUN_ALL_TESTS();
+	a.exec();
+}
+
